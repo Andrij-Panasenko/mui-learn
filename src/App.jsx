bfc447d326971395b76112bf6602d09f5de4e083
VladProtchenko <andrii.panasenko295@gmail.com>
2024-12-06 17:55:39 +0200

    validation and submiit form. MVP WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import style from './app.module.css';
 import { useState } from 'react';
-import { useFormControl } from '@mui/material/FormControl';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
@@ -10,14 +9,47 @@ import ProviderTheme from './components/ProviderTheme';
 
 export default function App() {
   const [isError, setIsError] = useState(false);
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+
+    setFormValues((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formValues.name || !formValues.email) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+      console.log('Form data:', formValues);
+    }
+  };
+
   return (
     <>
       <ProviderTheme>
         <Container fixed maxWidth="md">
           <Box
             component="form"
+            noValidate
             autoComplete="off"
-            sx={{ border: '1px solid red' }}
+            sx={{
+              border: '1px solid red',
+              borderRadius: '15px',
+              backgroundColor: 'rgba(0, 0, 0, 0.06)',
+            }}
+            onSubmit={handleSubmit}
           >
             <Grid
               container={true}
@@ -25,16 +57,19 @@ export default function App() {
               columns={12}
               alignItems={'center'}
               justifyContent={'center'}
-              sx={{ padding: '10px' }}
+              sx={{ padding: '20px' }}
             >
               <Grid item xs={12} md={6}>
                 <TextField
                   fullWidth
                   required
                   error={isError}
+                  helperText={isError ? 'This field is required' : ' '}
                   id="name"
                   label="Name"
                   placeholder="e.g., John Doe"
+                  value={formValues.name}
+                  onChange={handleChange}
                 />
               </Grid>
 
@@ -43,37 +78,44 @@ export default function App() {
                   fullWidth
                   required
                   error={isError}
+                  helperText={isError ? 'This field is required' : ' '}
                   id="email"
                   label="Email"
                   placeholder="Your email"
+                  value={formValues.email}
+                  onChange={handleChange}
                 />
               </Grid>
 
               <Grid item xs={12}>
                 <TextField
                   fullWidth
-                  error={isError}
                   id="phone"
+                  helperText=" "
                   label="Phone number"
                   placeholder="Your phone number"
+                  value={formValues.phone}
+                  onChange={handleChange}
                 />
               </Grid>
 
               <Grid item xs={12}>
                 <TextField
                   fullWidth
-                  error={isError}
+                  helperText=" "
                   id="address"
                   label="Address"
                   placeholder="Your phone address"
+                  value={formValues.address}
+                  onChange={handleChange}
                 />
               </Grid>
 
-              {/* <Grid>
+              <Grid item>
                 <Button type="submit" variant="contained">
                   Submit form
                 </Button>
-              </Grid> */}
+              </Grid>
             </Grid>
           </Box>
         </Container>
